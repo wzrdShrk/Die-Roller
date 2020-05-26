a4de764c94d1298d6c37f24b6bb1f5691e321304
wzrdShrk <wilh1206@gmail.com>
2020-05-26 16:05:14 -0500

    add compression

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var compression = require('compression')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,6 +10,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(compression())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
